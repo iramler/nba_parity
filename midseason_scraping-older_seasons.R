553674a356a53fad1fb163242ff3f66504aad9e4
iramler <iramler@users.noreply.github.com>
2023-06-23 14:25:00 -0400

    minor updates

--- midseason_scraping-older_seasons.R
+++ midseason_scraping-older_seasons.R
@@ -3,24 +3,27 @@
 library(rvest)
 library(tidyverse)
 
-standings_url <- "https://www.basketball-reference.com/friv/standings.fcgi?month=2&day=1&year=1965&lg_id=NBA&utm_source=direct&utm_medium=Share&utm_campaign=ShareTool"
+standings_url <- "https://www.basketball-reference.com/friv/standings.fcgi?month=2&day=1&year=1950&lg_id=NBA&utm_source=direct&utm_medium=Share&utm_campaign=ShareTool"
 
 standings <- read_html(standings_url) |> 
   html_elements(xpath = '//*[@id="all_standings"]/div')
 
-standings[[1]] |> 
-  html_table() |>
-  rename(
-    Team = X1, 
-    W = X2,
-    L = X3, 
-    `W/L%` = X4,
-    GB = X5,
-    PW = X6,
-    PL = X7,
-    `PS/G` = X8,
-    `PA/G` = X9
-    ) |>
-  mutate(W = as.numeric(W)) |> 
-  drop_na(W) |> 
-  type_convert(na = c("", "NA","—"))
+standings_tidy <-
+  standings[[1]] |> 
+    html_table() |>
+    rename(
+      Team = X1, 
+      W = X2,
+      L = X3, 
+      `W/L%` = X4,
+      GB = X5,
+      PW = X6,
+      PL = X7,
+      `PS/G` = X8,
+      `PA/G` = X9
+      ) |>
+    mutate(W = as.numeric(W)) |> 
+    drop_na(W) |> 
+    type_convert(na = c("", "NA","—"))
+
+standings_tidy
